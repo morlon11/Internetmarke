d4693a53961b47328739a8fdaa08cee60a11e45d
Robert Andraschko <r.andraschko@petex.net>
2025-09-11 16:51:45 +0200

    Fixed errors in build process

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 # development stage
-FROM node:18-alpine AS dev
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
-COPY . .
-CMD [ "npm", "run", "dev" ]
+#FROM node:18-alpine AS dev
+#WORKDIR /usr/src/app
+#COPY package*.json ./
+#RUN npm install
+#COPY . .
+#CMD [ "npm", "run", "dev" ]
 
 # production stage
 FROM node:18-alpine AS prod
 WORKDIR /usr/src/app
 COPY package*.json ./
+RUN rm -rf dist
 RUN npm install --only=production
 COPY . .
 CMD [ "node", "dist/index.js" ]
+
---
+++ test/dhl/auth.test.js
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { getAuthToken } from '../../src/dhl/auth.service.js';
+import { getCredentials } from '../../src/credentials.js';
+// global handler for unhandled promises
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection:', reason);
+    process.exit(1);
+});
+async function testAuthService() {
+    console.log('ID: ' +
+        getCredentials().client_id +
+        ', Secret: ' +
+        getCredentials().client_secret +
+        ', Usert: ' +
+        getCredentials().username +
+        ', Password: ' +
+        getCredentials().password);
+    if (!getCredentials().client_id ||
+        !getCredentials().client_secret ||
+        !getCredentials().username ||
+        !getCredentials().password) {
+        console.error('Fehler: Die Authentifizierungsdaten fehlen in den Umgebungsvariablen. Bitte stellen Sie sicher, dass Ihre .env-Datei korrekt ist.');
+        process.exit(1);
+    }
+    try {
+        console.log('Try to get an auth token...');
+        const token = await getAuthToken();
+        if (token) {
+            console.log('Here the received token:', token);
+            console.log('Test was successful.');
+        }
+        else {
+            console.error('Error: No token received.');
+        }
+    }
+    catch (error) {
+        let errorMessage = 'Ein unbekannter Fehler ist aufgetreten.';
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        console.error('Test fehlgeschlagen:', errorMessage);
+        process.exit(1);
+    }
+}
+testAuthService();
+//# sourceMappingURL=auth.test.js.map
